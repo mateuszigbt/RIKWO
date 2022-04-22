dd8125a70a3a310917c51c568c8695cc208c7562
Matuesz Niebora <mniebora@gmail.com>
2022-04-22 22:45:42 +0200

    Strona startowa restauracji oraz grafika

--- Restauracja/app/src/main/java/com/example/restauracja/Activity/StartActivity.java
+++ Restauracja/app/src/main/java/com/example/restauracja/Activity/StartActivity.java
@@ -16,6 +16,6 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final RelativeLayout relativeLayout;
-
+        String test= "test";
     }
 }
