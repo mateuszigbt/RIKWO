b8d3225b8ad668e046b8e45b9df7edea0723ac26
Matuesz Niebora <mniebora@gmail.com>
2022-04-22 22:42:24 +0200

    Strona startowa restauracji

---
+++ Restauracja/app/src/main/java/com/example/restauracja/Activity/StartActivity.java
@@ -0,0 +1,21 @@
+package com.example.restauracja.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.example.restauracja.R;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final RelativeLayout relativeLayout;
+
+    }
+}
